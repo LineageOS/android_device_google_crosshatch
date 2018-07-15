7ee7a631d4c7939356e6bd139d32b498d55f14aa
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-15 20:16:49 +0200

    crosshatch: Specify device has a notch
    Change-Id: Ib912728020f85c820b10d5102365dc37f9a54342

--- crosshatch/device-lineage.mk
+++ crosshatch/device-lineage.mk
@@ -4,4 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/crosshatch/overlay-lineage
+
 $(call inherit-product, device/google/crosshatch/device-lineage.mk)
