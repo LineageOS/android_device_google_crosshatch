8b680b3155781763a9407728f61cf6953b06cc05
razorloves <razorloves@gmail.com>
2018-09-02 23:30:11 -0600

    b1c1: Set OPA property to enable google assistant
    Change-Id: I741171219d64a28d56531f063f95f3f6f6cf81e6

--- device-lineage.mk
+++ device-lineage.mk
@@ -2,6 +2,9 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Google Assistant
+PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
+
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
