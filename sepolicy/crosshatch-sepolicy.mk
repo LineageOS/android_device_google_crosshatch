49557d831c7d671dda40e9f316a0a77e5a0603f3
Kevin F. Haggerty <haggertk@lineageos.org>
2018-11-24 14:38:00 -0700

    crosshatch: Update sepolicy path after subtree-ing
    Change-Id: Ib8824df678409a65c315748b7f6b40a29578b774

--- sepolicy/crosshatch-sepolicy.mk
+++ sepolicy/crosshatch-sepolicy.mk
@@ -1,13 +1,13 @@
-PRODUCT_PUBLIC_SEPOLICY_DIRS := device/google/crosshatch-sepolicy/public
-PRODUCT_PRIVATE_SEPOLICY_DIRS := device/google/crosshatch-sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS := device/google/crosshatch/sepolicy/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS := device/google/crosshatch/sepolicy/private
 
 # vendors
-BOARD_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/qcom/common
-BOARD_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/qcom/sdm845
-BOARD_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/google
-BOARD_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/verizon
+BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy/vendor/qcom/common
+BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy/vendor/qcom/sdm845
+BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy/vendor/google
+BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy/vendor/verizon
 
 # vendors for backward compatibility
 ifeq ($(PRODUCT_USE_QC_SPECIFIC_SYMLINKS), true)
-BOARD_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/qcom/compat
+BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy/vendor/qcom/compat
 endif
