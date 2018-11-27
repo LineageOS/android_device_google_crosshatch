4d4260001cea5baf6514dbb5437001bd0ca1073e
Ethan Chen <intervigil@gmail.com>
2018-11-26 23:36:11 -0800

    b1c1: Initial Lineage bringup
    Change-Id: Ic4b0b00cf511112839a8d25e095cd0a849eca26b

---
+++ blueline/BoardConfigLineage.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2018-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include device/google/crosshatch/BoardConfigLineage.mk
---
+++ crosshatch/device-lineage.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2018-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/google/crosshatch/device-lineage.mk)
