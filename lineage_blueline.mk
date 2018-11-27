abe78689d197cbc1097ef45cd60247f1e0ad4803
Ethan Chen <intervigil@gmail.com>
2018-11-26 23:36:11 -0800

    crosshatch: Initial Lineage bringup
    Change-Id: Ic4b0b00cf511112839a8d25e095cd0a849eca26b

---
+++ lineage_blueline.mk
@@ -0,0 +1,26 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2160
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/blueline/aosp_blueline.mk)
+
+-include device/google/blueline/blueline/device-lineage.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_blueline
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 3
+TARGET_MANUFACTURER := HTC
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=blueline \
+    PRIVATE_BUILD_DESC="blueline-user 9 PPR2.181005.003 4984323 release-keys"
+
+BUILD_FINGERPRINT := google/blueline/blueline:9/PPR2.181005.003/4984323:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/crosshatch/blueline-vendor.mk)
