bd296e8f6b704260d74b458eaae8e460b7eb32b2
Ethan Chen <intervigil@gmail.com>
2018-11-28 21:46:17 -0800

    crosshatch: Add proprietary file listing
    Change-Id: I23de72524c5697ba3f6781c45ef4da804bc728b7

---
+++ setup-makefiles.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+VENDOR=google
+DEVICE=crosshatch
+
+INITIAL_COPYRIGHT_YEAR=2017
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Write custom header to allow blueline to inherit
+function write_crosshatch_headers() {
+    write_header "$ANDROIDMK"
+
+    cat << EOF >> "$ANDROIDMK"
+LOCAL_PATH := \$(call my-dir)
+
+EOF
+    cat << EOF >> "$ANDROIDMK"
+ifneq (\$(filter crosshatch blueline,\$(TARGET_DEVICE)),)
+
+EOF
+
+    write_header "$BOARDMK"
+    write_header "$PRODUCTMK"
+}
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+
+# Copyright headers and guards
+write_crosshatch_headers
+
+# The standard blobs
+write_makefiles "$MY_DIR"/device-proprietary-files.txt
+
+cat << EOF >> "$ANDROIDMK"
+
+EOF
+
+# Finish
+write_footers
