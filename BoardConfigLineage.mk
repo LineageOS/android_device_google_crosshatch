7ec8cb9ce82c198b1cee9e8e278cf3871b2cd6f7
Ethan Chen <intervigil@gmail.com>
2018-11-28 20:44:43 -0800

    crosshatch: Build disabled vbmeta image
    Change-Id: I40a6218f36ba2439855b14506bad726a49de774e

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,5 +1,7 @@
 # Common board config for crosshatch, blueline
 
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ARCH := arm64
