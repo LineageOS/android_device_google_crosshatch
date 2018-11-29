96c0eb38c1cd360117b11323ee28ec169117202a
Ethan Chen <intervigil@gmail.com>
2018-11-28 20:41:20 -0800

    crosshatch: Disable userdebug diagnostics
    * We can't make use of most of the debug features on userdebug builds.
    
    Change-Id: I31df2090b31243b742c4e82dc08cd8491dada54f

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -271,7 +271,7 @@ DEVICE_MATRIX_FILE := device/google/crosshatch/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/google/crosshatch/device_framework_matrix.xml
 
 # Userdebug only Vendor Interface Manifest
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 DEVICE_FRAMEWORK_MANIFEST_FILE += device/google/crosshatch/framework_manifest_userdebug.xml
 DEVICE_MATRIX_FILE += device/google/crosshatch/compatibility_matrix_userdebug.xml
 endif
