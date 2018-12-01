7be642f99bd749762d346302a5c1b439ac2d5def
Ethan Chen <intervigil@gmail.com>
2018-11-30 23:28:49 -0800

    b1c1: Add AOSP RCS packages
    Change-Id: Ic16d3b92820d246db73ad70ccaa0f4bc7f50e515

--- device-lineage.mk
+++ device-lineage.mk
@@ -10,3 +10,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# RCS
+PRODUCT_PACKAGES += \
+    PresencePolling \
+    RcsService
