5cc01de04c5b22133f4b409119a36a4d7adf19ff
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    crosshatch: Declare EUICC permission
    Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,3 +1,7 @@
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
