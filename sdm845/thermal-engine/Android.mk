e8dac1747483a9cbe3b6ee8d8d5394f6bbfc0e75
Ethan Chen <intervigil@gmail.com>
2018-12-04 22:45:38 -0800

    b1c1: Use vendor/qcom/opensource/thermal-engine headers
    Change-Id: Ibc4f5d1702c52a66de397b486b4e27ae482a258e

--- sdm845/thermal-engine/Android.mk
+++ sdm845/thermal-engine/Android.mk
@@ -4,6 +4,7 @@
 ################################################################################
 
 LOCAL_PATH:= $(call my-dir)
+<<<<<<< HEAD   (684445 b1c1: Set date for vendor and boot SPL)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libThermal_headers
@@ -12,3 +13,5 @@ LOCAL_LICENSE_CONDITIONS := notice
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_HEADER_LIBRARY)
+=======
+>>>>>>> CHANGE (0cfe5c b1c1: Use vendor/qcom/opensource/thermal-engine headers)
