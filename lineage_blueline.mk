b52d6a50a791e2113e4b00374dc1bacd350581b8
Ethan Chen <intervigil@gmail.com>
2019-03-08 22:55:03 -0800

    crosshatch: Update fingerprint to March
    Change-Id: If65d8eccdf99a377d2b935d44517ce795f63b893

--- lineage_blueline.mk
+++ lineage_blueline.mk
@@ -19,8 +19,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=blueline \
-    PRIVATE_BUILD_DESC="blueline-user 9 PQ2A.190205.001 5163636 release-keys"
+    PRIVATE_BUILD_DESC="blueline-user 9 PQ2A.190305.002 5240760 release-keys"
 
-BUILD_FINGERPRINT := google/blueline/blueline:9/PQ2A.190205.001/5163636:user/release-keys
+BUILD_FINGERPRINT := google/blueline/blueline:9/PQ2A.190305.002/5240760:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
--- lineage_crosshatch.mk
+++ lineage_crosshatch.mk
@@ -19,8 +19,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=crosshatch \
-    PRIVATE_BUILD_DESC="crosshatch-user 9 PQ2A.190205.001 5163636 release-keys"
+    PRIVATE_BUILD_DESC="crosshatch-user 9 PQ2A.190305.002 5240760 release-keys"
 
-BUILD_FINGERPRINT := google/crosshatch/crosshatch:9/PQ2A.190205.001/5163636:user/release-keys
+BUILD_FINGERPRINT := google/crosshatch/crosshatch:9/PQ2A.190305.002/5240760:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
