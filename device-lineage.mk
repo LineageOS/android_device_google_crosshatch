af33965f530d147405228e127da7d84b38ad4cec
Nolen Johnson <johnsonnolen@gmail.com>
2019-04-01 18:23:23 +0000

    crosshatch: Build Snap & Enable Camera APIv2
    * Blueline inherits this as well.
    
    Change-Id: I0a1e113a908db25fcbc208871f5e5739cb874446

--- device-lineage.mk
+++ device-lineage.mk
@@ -2,8 +2,15 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/overlay-lineage
+
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
     PresencePolling \
     RcsService
+
+# Snap Camera
+PRODUCT_PACKAGES += \
+    Snap
