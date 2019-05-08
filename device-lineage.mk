94e7d13951295b6a2e4d06eadbfc3c230b2d33e7
Nolen Johnson <johnsonnolen@gmail.com>
2019-05-08 08:48:32 +0000

    crosshatch: Build the Vendor Image
    Change-Id: Iee5d6dfb6c51d3ff107728fb33a8793212ef6069

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,3 +1,6 @@
+AB_OTA_PARTITIONS += \
+    vendor
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
