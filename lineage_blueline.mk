ebb578fe9c280f00c365544bbcfaf1fd84bb6dbd
razorloves <razorloves@gmail.com>
2019-05-26 01:56:08 -0500

    b1c1: Setup separate extract scripts
    There are differences between the device's vendor
    images so we need to separate the extract files.
    
    Change-Id: I2e959f2b9d7db4aa8001ea73cd50ce7ce7dc6320

--- lineage_blueline.mk
+++ lineage_blueline.mk
@@ -23,4 +23,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := google/blueline/blueline:10/QP1A.191105.003/5899767:user/release-keys
 
-$(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
+$(call inherit-product-if-exists, vendor/google/blueline/blueline-vendor.mk)
