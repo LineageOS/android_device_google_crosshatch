53722d0ece1c83e87ae36fb5644786c54fd78e40
razorloves <razorloves@gmail.com>
2019-05-26 04:14:33 -0500

    b1c1: Add missing packages and props for vendor image
    Change-Id: I88bdf8b2522b751c9490820b0ef55ee53dd35d1d

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,6 +1,19 @@
+# Display
+PRODUCT_PACKAGES += \
+    libdisplayconfig
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    media.mediadrmservice.enable=true
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+
+# IPA config
+PRODUCT_PACKAGES += \
+    IPACM_cfg.xml
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/overlay-lineage
@@ -15,8 +28,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# Utilities
+PRODUCT_PACKAGES += \
+    libjson \
+    libtinyxml
+
 # vendor.img
 AB_OTA_PARTITIONS += vendor
+
+# WiFi
+PRODUCT_PACKAGES += \
+    libwifi-hal-qcom
 
 WITH_GMS_FI := true
 
