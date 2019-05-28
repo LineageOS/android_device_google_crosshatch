c1fa336704fd382018cb5adf08ee2f586d65a886
Felix <google@ix5.org>
2019-05-28 13:42:39 +0200

    Makefile: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated
    
    Change-Id: Ia66f8462d7b7c546d8bd4d25c6fdfd22b1726841
    Signed-off-by: Felix <google@ix5.org>

--- crosshatch-sepolicy.mk
+++ crosshatch-sepolicy.mk
@@ -2,10 +2,10 @@ PRODUCT_PUBLIC_SEPOLICY_DIRS := device/google/crosshatch-sepolicy/public
 PRODUCT_PRIVATE_SEPOLICY_DIRS := device/google/crosshatch-sepolicy/private
 
 # vendors
-BOARD_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/qcom/common
-BOARD_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/qcom/sdm845
-BOARD_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/google
-BOARD_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/verizon
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/qcom/common
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/qcom/sdm845
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/google
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/crosshatch-sepolicy/vendor/verizon
 
 # vendors for backward compatibility
 ifeq ($(PRODUCT_USE_QC_SPECIFIC_SYMLINKS), true)
