c154fdb6d4bf362acd646ac9b30259b65589275d
razorloves <razorloves@gmail.com>
2019-08-11 01:22:03 -0500

    b1c1: Remove Google platform signature overlays
    The vendor and product images are being built, so no longer needed.
    
    Partial revert of commit 3af9e6568dd39e5e98cf95d8d40228984491036d.
    
    Change-Id: I95715c9e6225494f7500902f3ae42b5d0155209f

--- blueline/device-lineage.mk
+++ blueline/device-lineage.mk
@@ -1,5 +1,1 @@
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/blueline/overlay-lineage
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += device/google/crosshatch/blueline/overlay-lineage/lineage-sdk
-
 $(call inherit-product, device/google/crosshatch/device-lineage.mk)
