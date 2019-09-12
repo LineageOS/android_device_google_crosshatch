0375d58d1479061fca6ad464523055cddf3557f4
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-11 22:33:08 -0700

    crosshatch: Remove kernel cross compile prefix
    Change-Id: I4e96de0e876dce04fed9cc45b573123e63d0a770

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -8,7 +8,6 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := lineageos_crosshatch_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 
 -include vendor/google/crosshatch/BoardConfigVendor.mk
