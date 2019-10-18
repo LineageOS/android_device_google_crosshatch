e86964675f812babfe80739e6562c11828f46d06
razorloves <razorloves@gmail.com>
2019-10-18 00:06:22 -0500

    b1c1: Move eUICC permissions xml to /product
    All eUICC items were moved to /product in Q.
    
    Change-Id: I614f86b16f61959c47465863ab1d5d55030209ab

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,6 +1,6 @@
 # EUICC
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
