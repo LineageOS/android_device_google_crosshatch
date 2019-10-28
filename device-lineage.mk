361d1a9e758de5c4e0d4bf3415a94da8c4108208
razorloves <razorloves@gmail.com>
2019-10-28 02:05:01 -0500

    b1c1: Get correct IPACM_cfg.xml
    Get the xml from the sdm845 data/ipacfg-mgr hal instead of msm8998.
    
    Change-Id: I3d1ba4e7c536d328965482d7a660f535d15f90a7

--- device-lineage.mk
+++ device-lineage.mk
@@ -14,6 +14,10 @@ PRODUCT_COPY_FILES += \
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
+# IPA config
+PRODUCT_PACKAGES += \
+    IPACM_cfg.xml
+
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
