d4dc3edd7da04ac37a6991044614017d68fcef63
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    b1c1: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- blueline/device-lineage.mk
+++ blueline/device-lineage.mk
@@ -1,4 +1,5 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/blueline/overlay-lineage
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += device/google/crosshatch/blueline/overlay-lineage/lineage-sdk
 
 $(call inherit-product, device/google/crosshatch/device-lineage.mk)
