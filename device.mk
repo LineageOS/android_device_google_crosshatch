f25ebd182b3e2b9443881fa7e7b6a89020236014
razorloves <razorloves@gmail.com>
2020-03-13 23:26:49 -0500

    b1c1: Remove adb secure prop
    This is preventing eng builds from being adb insecure.
    It's also conditionally handled in vendor repo.
    
    Change-Id: Idc77c39a5e2311ea1fcb273c6140afe655c574c9

--- device.mk
+++ device.mk
@@ -817,8 +817,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.svn=26
 
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.adb.secure=1
+#PRODUCT_PRODUCT_PROPERTIES += \
+#    ro.adb.secure=1
 
 # pixel atrace HAL
 PRODUCT_PACKAGES += \
