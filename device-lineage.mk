a99b52d0c495481151bd21ec875f449a506d86cb
Dan Pasanen <dan.pasanen@gmail.com>
2020-07-17 20:41:45 -0400

    b1c1: Build LiveDisplay 2.0 HAL
    * Lets use the common solution, and change our supported
      interfaces while we're at it:
     * Our panel (by default) uses AOSP color profiles.
     * Our panel does support HBM (right now), but elsewhere.
     * Our panel does get along with Color Calibration &
       Reading Mode, which are done SDK side, and require
       no HIDL manifest entries.
     * Our panel does get along with Picture Adjustment
       features, so leave IPictureAdjustment in place.
     * AOSP declares `ro.vendor.display.foss` but we don't
       have support for IAdaptiveBacklight, so don't declare.
    
    Change-Id: Ia96134e0a4bb06269b48671a87991d5e7a53d9ed

--- device-lineage.mk
+++ device-lineage.mk
@@ -11,6 +11,10 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # RCS
 PRODUCT_PACKAGES += \
     PresencePolling \
