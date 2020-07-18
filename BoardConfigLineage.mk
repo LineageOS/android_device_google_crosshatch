d360a7489e66172536a09de3ef5ae82e0b19e6ec
Dan Pasanen <dan.pasanen@gmail.com>
2020-07-17 20:41:45 -0400

    b1c1: Build LiveDisplay 2.0 HAL
    * Lets use the common solution, and change our supported
      interfaces while we're at it:
     * Our panel (by default) uses AOSP color profiles.
     * Our panel does support HBM (right now), but elsewhere.
     * Our panel does get along with Color Calibration &
       Reading Mode, which are done SDK side, and require
       no HIDL manifest entries.
     * Our panel does get along with Picture Adjustment
       features, so leave IPictureAdjustment in place.
     * AOSP declares `ro.vendor.display.foss` but we don't
       have support for IAdaptiveBacklight, so don't declare.
    
    Change-Id: Ia96134e0a4bb06269b48671a87991d5e7a53d9ed

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -18,6 +18,10 @@ TARGET_KERNEL_CONFIG := b1c1_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 TARGET_NEEDS_DTBOIMAGE := true
 
+# Manifests
+DEVICE_MANIFEST_FILE += device/google/crosshatch/lineage_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+
 # Partitions
 AB_OTA_PARTITIONS += \
     vendor
@@ -35,6 +39,10 @@ BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 1250140160
 endif
 BOARD_SYSTEM_EXTIMAGE_PARTITION_RESERVED_SIZE := 30720000
 BOARD_VENDORIMAGE_PARTITION_RESERVED_SIZE := 30720000
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy-lineage/dynamic
+BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy-lineage/vendor
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
