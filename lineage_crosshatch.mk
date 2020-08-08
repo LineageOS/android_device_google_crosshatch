3ef026a8c3d8b250edab15b86ee78c91bb639602
razorloves <razorloves@gmail.com>
2020-08-08 11:18:19 -0500

    b1c1: Update build fingerprint to Aug 2020 QQ3A.200805.001
    Change-Id: I2d251144e3aa079634680de3334361c95f4846d5

--- lineage_crosshatch.mk
+++ lineage_crosshatch.mk
@@ -19,8 +19,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=crosshatch \
-    PRIVATE_BUILD_DESC="crosshatch-user 10 QQ3A.200705.002 6506677 release-keys"
+    PRIVATE_BUILD_DESC="crosshatch-user 10 QQ3A.200805.001 6578210 release-keys"
 
-BUILD_FINGERPRINT := google/crosshatch/crosshatch:10/QQ3A.200705.002/6506677:user/release-keys
+BUILD_FINGERPRINT := google/crosshatch/crosshatch:10/QQ3A.200805.001/6578210:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
