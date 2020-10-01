31e28c2d4e663e2ba368802457aadd78438602a6
razorloves <razorloves@gmail.com>
2020-10-01 07:12:38 -0500

    b1c1: Update vendor packages & blobs for R
     * The chre binary was opensourced in R, so compile it now.
     * Various other blob additions and removals.
     * Build what we can.
    
    Change-Id: Id03b3b4faa7110b02243340de97fa9c4a0c6ae8f

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,6 +1,23 @@
+# AV media
+PRODUCT_PACKAGES += \
+    libavservices_minijail_vendor:32 \
+    libcodec2_hidl@1.0.vendor:32 \
+    libcodec2_vndk.vendor:64 \
+    libmediaplayerservice
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    com.qualcomm.qti.bluetooth_audio@1.0 \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor
+
+# Context Hub Runtime Environment
+PRODUCT_PACKAGES += \
+    chre
+
 # Display
 PRODUCT_PACKAGES += \
-    libdisplayconfig
+    libdisplayconfig \
+    vendor.display.config@1.8
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -14,8 +31,32 @@ PRODUCT_COPY_FILES += \
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport.vendor \
+    libhwbinder.vendor
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.identity-support-lib.vendor:64 \
+    libnos_client_citadel:64 \
+    libcppbor.vendor:64 \
+    libteeui_hal_support.vendor:64 \
+    nos_app_avb:64 \
+    nos_app_identity:64 \
+    nos_app_keymaster:64 \
+    nos_app_weaver:64 \
+    vendor.qti.hardware.cryptfshw@1.0 \
+    vendor.qti.hardware.cryptfshw@1.0.vendor
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/overlay-lineage
+
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@1.0 \
+    vendor.qti.hardware.perf@1.0.vendor \
+    vendor.qti.hardware.perf@2.0.vendor
 
 # Trust HAL
 PRODUCT_PACKAGES += \
@@ -24,8 +65,10 @@ PRODUCT_PACKAGES += \
 # Utilities
 PRODUCT_PACKAGES += \
     libjson \
+    libprotobuf-cpp-full-vendorcompat \
     libtinyxml
 
 # WiFi
 PRODUCT_PACKAGES += \
+    libwifi-hal:64 \
     libwifi-hal-qcom
