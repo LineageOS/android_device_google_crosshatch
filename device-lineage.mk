9a217ebff166d8a8625928c8926aa8eff45629d5
Danny Lin <danny@kdrag0n.dev>
2020-11-05 15:01:18 -0800

    b1c1: Build Active Edge service (ElmyraService)
    This package provides comprehensive support for the Active Edge squeeze
    gesture supported on this device.
    
    Change-Id: I2158c22e727e293e3f02b9ddf614a943848ad587

--- device-lineage.mk
+++ device-lineage.mk
@@ -7,6 +7,10 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
+# Elmyra
+PRODUCT_PACKAGES += \
+    ElmyraService
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
