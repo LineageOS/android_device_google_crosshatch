9a36a9b438a764dbde28f6b90bd46acdddce918b
Cole Faust <colefaust@google.com>
2021-12-21 11:43:54 -0800

    Inline usages of LOCAL_STEM
    Having no variables in include/inherit-product statements
    makes the conversion to starlark cleaner.
    
    Bug: 211659921
    Test: Presubmits
    Change-Id: I9855bf74876f1895429fb5e5c173f619d29c74aa

--- self-extractors_blueline/root/proprietary/device-vendor.mk
+++ self-extractors_blueline/root/proprietary/device-vendor.mk
@@ -12,7 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_STEM := blueline/device-partial.mk
-
-$(call inherit-product-if-exists, vendor/google_devices/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/qcom/$(LOCAL_STEM))
+$(call inherit-product-if-exists, vendor/google_devices/blueline/device-partial.mk)
+$(call inherit-product-if-exists, vendor/qcom/blueline/device-partial.mk)
