9c02ff3de6eb6f92d0d90c2b74401019c7cfb85f
Michael Bestas <mkbestas@lineageos.org>
2022-01-13 23:08:06 +0200

    [SQUASH] Merge tag 'android-12.0.0_r25' into staging/lineage-19.0_merge-android-12.0.0_r25
    Android 12.0.0 release 25
    
    # -----BEGIN PGP SIGNATURE-----
    #
    # iF0EABECAB0WIQRDQNE1cO+UXoOBCWTorT+BmrEOeAUCYdTCUQAKCRDorT+BmrEO
    # eHtGAJ4w/4MYyLzyxGGU0YfSjxAF7yuYDgCdHWUHR5fEuwu8lm71uRb0ooPNU9M=
    # =wEIu
    # -----END PGP SIGNATURE-----
    # gpg: Signature made Tue Jan  4 23:55:29 2022 EET
    # gpg:                using DSA key 4340D13570EF945E83810964E8AD3F819AB10E78
    # gpg: Good signature from "The Android Open Source Project <initial-contribution@android.com>" [marginal]
    # gpg: initial-contribution@android.com: Verified 1026 signatures in the past
    #      2 months.  Encrypted 4 messages in the past 3 days.
    # gpg: WARNING: This key is not certified with sufficiently trusted signatures!
    # gpg:          It is not certain that the signature belongs to the owner.
    # Primary key fingerprint: 4340 D135 70EF 945E 8381  0964 E8AD 3F81 9AB1 0E78
    
    # By Neha Jain (1) and Paul Scovanner (1)
    # Via Android Build Coastguard Worker
    * tag 'android-12.0.0_r25':
      Update Crosshatch SVN to 60
      Update Crosshatch SVN to 59
    
    Change-Id: I38d320aa92985ac03392328f1c8684247946bb67

--- device.mk
+++ device.mk
@@ -839,7 +839,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=58
+    ro.vendor.build.svn=60
 
 # pixel atrace HAL
 PRODUCT_PACKAGES += \
