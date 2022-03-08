97ef9cccff5825f1ae76df3f694c40f93ffecb9d
Bill Yi <byi@google.com>
2022-03-08 13:53:47 -0800

    Add new notice file build logic
    BUG: 223289891
    
    Change-Id: I9cadc3074a51d47d3babc6fe63cf91bc79d4bf31
    Merged-in: I9cadc3074a51d47d3babc6fe63cf91bc79d4bf31

--- self-extractors_blueline/qcom/staging/Android.mk
+++ self-extractors_blueline/qcom/staging/Android.mk
@@ -1,5 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
+$(eval $(call declare-copy-files-license-metadata,vendor/qcom/blueline,:qcom,legacy_proprietary,proprietary,vendor/qcom/sargo/LICENSE,))
+$(eval $(call declare-copy-files-license-metadata,vendor/qcom/blueline,.jar,legacy_proprietary,proprietary,vendor/qcom/sargo/LICENSE,))
+$(eval $(call declare-copy-files-license-metadata,vendor/qcom/blueline,.xml,legacy_proprietary,proprietary,vendor/qcom/sargo/LICENSE,))
+
 ifneq ($(filter blueline, $(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
