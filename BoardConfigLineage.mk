c2dc2053a69e1fd7a30a7f944976297a806febb1
Georg Veichtlbauer <georg@vware.at>
2022-03-10 16:03:55 +0100

    b1c1: Remove CLANG compile flag
    Defaults to true now project-wide.
    
    Change-Id: I1e0ff5adadc889d81c90c816457a72d2621e4b5a

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -12,7 +12,6 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
-TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := b1c1_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 TARGET_NEEDS_DTBOIMAGE := true
