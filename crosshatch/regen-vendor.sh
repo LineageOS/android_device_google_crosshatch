34cbb2698ca83a305b547bcadf913583536517b0
Chirayu Desai <chirayudesai1@gmail.com>
2022-04-05 03:32:42 +0530

    Fix regen-vendor.sh
    Change-Id: I33206a028a863d9849d49a6b0ed44661e03d6e0c

--- crosshatch/regen-vendor.sh
+++ crosshatch/regen-vendor.sh
@@ -1113,6 +1113,9 @@ VENDOR_SKIP_FILES=(
     "firmware/TEST_MAPPING"
 )
 
+# Initialize the helper
+setup_vendor_deps "${ANDROID_ROOT}"
+
 generate_prop_list_from_image "${_input_image}" "${_output_file}" VENDOR_SKIP_FILES
 
 # Fixups
