a1020d25b4898d1d1eb63960c7ff4389b2b92f5c
Paul Scovanner <pscovanner@google.com>
2022-05-17 20:49:46 +0000

    Update Crosshatch SVN to 64
    Bug: 209706659
    Change-Id: I35bbce73b6f8e85fce1f60d0cf2dd4f6a2d78d6c
    (cherry picked from commit 2681834d5946834d3f7971953ae4a89fe9137937)
    Merged-In: I35bbce73b6f8e85fce1f60d0cf2dd4f6a2d78d6c

--- device.mk
+++ device.mk
@@ -849,7 +849,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=63
+    ro.vendor.build.svn=64
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.adb.secure=1
