0e090d7c89229d79681e32a2c55a30afef805adc
Michael Bestas <mkbestas@lineageos.org>
2022-06-16 08:47:33 +0300

    crosshatch: Override rounded_corner_radius_top when hiding cutout
    Make it the same as rounder_corner_radius_bottom
    
    Change-Id: Ic88720aa0c2cbac5bb34d6c81bf3f3c4704090e2

--- crosshatch/device-lineage.mk
+++ crosshatch/device-lineage.mk
@@ -7,4 +7,7 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/crosshatch/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    device/google/crosshatch/crosshatch/overlay-lineage/frameworks/base/packages/overlays/NoCutoutOverlay
+
 $(call inherit-product, device/google/crosshatch/device-lineage.mk)
