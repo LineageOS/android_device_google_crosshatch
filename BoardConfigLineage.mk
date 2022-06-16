3bccdc2a08325f846b47e2c6b8b77d3fdc8a197d
Michael Bestas <mkbestas@lineageos.org>
2022-06-16 07:41:37 +0300

    b1c1: Remove MKDTIMG override
    mkdtimg is built from source
    
    Change-Id: I4bacc6f22583dbdf74493f7528c12caa09a3b1fc

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -9,9 +9,7 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
 TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
-    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
 TARGET_KERNEL_CONFIG := b1c1_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 TARGET_NEEDS_DTBOIMAGE := true
