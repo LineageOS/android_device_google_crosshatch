1962f7d2c5e82cae4eea7b45fdaf5960328aa739
Code Able <codable@gmail.com>
2022-06-18 09:23:33 +0800

    blueline: fix blob for ChinaTelecom
    This commit enables ChinaTelecom support for blueline.
    
    ref:
    https://github.com/wangdaning/Chinese-Carrier-Pixel-3-xl-LTE-VoLTE-Enabler
    
    Change-Id: I8ca1b5d4292d303f810485d99fe8b5ff6957f493

--- blueline/extract-files.sh
+++ blueline/extract-files.sh
@@ -24,6 +24,34 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/rfs/msm/mpss/readonly/vendor/mbn/mcfg_sw/mbn_sw.txt)
+            grep -q 'China/CT' "${2}" || echo \
+'mcfg_sw/generic/China/CMCC/Commercial/Volte_OpenMkt/mcfg_sw.mbn
+mcfg_sw/generic/China/CMCC/Lab/AGNSS_LocTech/mcfg_sw.mbn
+mcfg_sw/generic/China/CMCC/Lab/Conf_VoLTE/mcfg_sw.mbn
+mcfg_sw/generic/China/CMCC/Lab/EPS_Only/mcfg_sw.mbn
+mcfg_sw/generic/China/CMCC/Lab/LPP_LocTech/mcfg_sw.mbn
+mcfg_sw/generic/China/CMCC/Lab/Nsiot_VoLTE/mcfg_sw.mbn
+mcfg_sw/generic/China/CMCC/Lab/RRLP_LocTech/mcfg_sw.mbn
+mcfg_sw/generic/China/CMCC/Lab/TGL_Comb_Attach/mcfg_sw.mbn
+mcfg_sw/generic/China/CMCC/Lab/W_IRAT_Comb_Attach/mcfg_sw.mbn
+mcfg_sw/generic/China/CT/Commercial/hVoLTE_OpenMkt/mcfg_sw.mbn
+mcfg_sw/generic/China/CT/Commercial/OpenMkt/mcfg_sw.mbn
+mcfg_sw/generic/China/CT/Commercial/VoLTE_OpenMkt/mcfg_sw.mbn
+mcfg_sw/generic/China/CT/Lab/CTA/mcfg_sw.mbn
+mcfg_sw/generic/China/CT/Lab/TEST/mcfg_sw.mbn
+mcfg_sw/generic/China/CT/Lab/TEST_EPS_ONLY/mcfg_sw.mbn
+mcfg_sw/generic/China/CT/Lab/TEST_NO_APN/mcfg_sw.mbn
+mcfg_sw/generic/China/CT/Lab/VoLTE_Conf/mcfg_sw.mbn
+mcfg_sw/generic/China/CU/Commercial/OpenMkt/mcfg_sw.mbn
+mcfg_sw/generic/China/CU/Commercial/VoLTE/mcfg_sw.mbn
+mcfg_sw/generic/China/CU/Lab/Test/mcfg_sw.mbn' >> "${2}"
+            ;;
+    esac
+}
+
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
