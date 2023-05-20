05d58b9faa94cc6dda1539e35d645bcebfa17573
Michael Bestas <mkbestas@lineageos.org>
2023-05-20 22:58:22 +0300

    crosshatch: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -16,6 +16,10 @@ TARGET_KERNEL_LLVM_BINUTILS := false
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 TARGET_NEEDS_DTBOIMAGE := true
 
+# Lineage Health
+TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
+TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
+
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/crosshatch/lineage_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
