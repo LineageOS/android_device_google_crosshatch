0af0de96966b7f3ca24d015ca9a17d31f485cebf
Michael Bestas <mkbestas@lineageos.org>
2023-05-20 22:58:22 +0300

    crosshatch: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- device-lineage.mk
+++ device-lineage.mk
@@ -27,6 +27,10 @@ PRODUCT_COPY_FILES += \
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
