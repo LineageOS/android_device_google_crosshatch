3ffa1e5e8c1676dfeb47e1ba783275ed12cc64b1
Michael Bestas <mkbestas@lineageos.org>
2023-07-29 13:18:46 +0300

    Revert "crosshatch: Build Lineage Health HAL"
    msm-4.9 doesn't have the google_battery kernel driver.
    
    This reverts commit 05d58b9faa94cc6dda1539e35d645bcebfa17573.
    
    Change-Id: Ibec737c60f8348f7eaaa4edad5f5183f4ad4ad49

--- device-lineage.mk
+++ device-lineage.mk
@@ -27,9 +27,6 @@ PRODUCT_COPY_FILES += \
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
 
-# Lineage Health
-include hardware/google/pixel/lineage_health/device.mk
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
