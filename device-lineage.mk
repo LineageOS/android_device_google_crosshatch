dadf265d65896e72793dc926a13500caa0658a59
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    b1c1: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device-lineage.mk
+++ device-lineage.mk
@@ -93,6 +93,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjson
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     libwifi-hal:64 \
