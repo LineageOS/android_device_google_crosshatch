859e5604f567e25873d79f8c45cb63cefc707fd4
Michael Bestas <mkbestas@lineageos.org>
2023-11-30 20:44:20 +0200

    b1c1: Add firmware extraction support
    Change-Id: If6cb6334a8f7b2fcc77c8947569eba1b2002190a

--- blueline/setup-makefiles.sh
+++ blueline/setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2021 The LineageOS Project
+# Copyright (C) 2017-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -33,6 +33,8 @@ write_headers
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 write_makefiles "${MY_DIR}/proprietary-files-carriersettings.txt" true
 write_makefiles "${MY_DIR}/proprietary-files-vendor.txt" true
+
+append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
 
 # Finish
 write_footers
