c518da68d35370a9631d6eab39132d66e396e7dd
LuK1337 <priv.luk@gmail.com>
2024-03-18 18:18:12 +0100

    b1c1: Use libcrypto-v33 from hardware/lineage/compat
    Because vndk prebuilt has SONAME set to libcrypto.so, it prevents linker
    from loading libcrypto.so built from source.
    
    Change-Id: Ic9e29f7b00b469163f48ea407fc9b5d5e90816c8

--- device-lineage.mk
+++ device-lineage.mk
@@ -150,5 +150,5 @@ PRODUCT_PACKAGES += \
     android.hardware.rebootescrow-V1-ndk_platform.vendor:64
 
 # VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v33.so
+PRODUCT_PACKAGES += \
+    libcrypto-v33
