b1628c63fb6d66e6bab9f94707d0ecb582ee5716
Aaron Kling <webgeek1234@gmail.com>
2024-08-14 01:44:45 -0500

    b1c1: Enable elf checks
    Change-Id: Ia6c9a381abb3384c77532d75e02fd84c841a4d1e

--- crosshatch/extract-files.sh
+++ crosshatch/extract-files.sh
@@ -16,8 +16,6 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../../.."
 
-export TARGET_ENABLE_CHECKELF=false
-
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
