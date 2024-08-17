c722798e8cfc727ddf73c328825c673e5d64ebe7
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 04:59:30 +0300

    b1c1: do not manually build dependencies
    Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b

--- device-lineage.mk
+++ device-lineage.mk
@@ -43,75 +43,19 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
-# Build necessary packages for product
-
-# Display
-PRODUCT_PACKAGES += \
-    vendor.display.config@1.0
-
 # Build necessary packages for vendor
-
-# Bluetooth
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
-    hardware.google.bluetooth.bt_channel_avoidance@1.0.vendor \
-    hardware.google.bluetooth.sar@1.0.vendor:64 \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor
 
 # CHRE
 PRODUCT_PACKAGES += \
     chre
 
-# Codec2
-PRODUCT_PACKAGES += \
-    android.hardware.media.c2@1.0.vendor:32 \
-    libavservices_minijail.vendor \
-    libcodec2_hidl@1.0.vendor:32 \
-    libcodec2_vndk.vendor \
-    libstagefright_bufferpool@2.0.1.vendor
-
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
 
-# Confirmation UI
-PRODUCT_PACKAGES += \
-    android.hardware.confirmationui@1.0.vendor:64 \
-    libteeui_hal_support.vendor:64
-
-# Display
-PRODUCT_PACKAGES += \
-    vendor.display.config@1.0.vendor \
-    vendor.display.config@1.1.vendor \
-    vendor.display.config@1.2.vendor \
-    vendor.display.config@1.3.vendor
-
-# HIDL
-PRODUCT_PACKAGES += \
-    libhidltransport.vendor \
-    libhwbinder.vendor
-
 # Identity credential
 PRODUCT_PACKAGES += \
-    android.hardware.identity-support-lib.vendor:64 \
     android.hardware.identity_credential.xml
-
-# Nos
-PRODUCT_PACKAGES += \
-    libkeymaster4support.vendor:64 \
-    libkeymaster4_1support.vendor:64 \
-    libcppbor_external.vendor \
-    libcrypto_shim.vendor
-
-# Json
-PRODUCT_PACKAGES += \
-    libjson
-
-# Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-full-3.9.1-vendorcompat \
-    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -124,64 +68,7 @@ PRODUCT_PACKAGES += \
 
 # VNDK FWK detect
 PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti.vendor
-
-# Wi-Fi
-PRODUCT_PACKAGES += \
-    android.hardware.wifi-V2-ndk.vendor:64 \
-    android.hardware.wifi@1.0.vendor:64 \
-    libwifi-hal:64 \
-    libwifi-hal-qcom \
-    libwifi-system-iface.vendor:64
-
-# Misc interfaces
-PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0.vendor \
-    android.frameworks.stats-V1-ndk.vendor:64 \
-    android.frameworks.stats@1.0.vendor:64 \
-    android.hardware.authsecret@1.0.vendor:64 \
-    android.hardware.biometrics.fingerprint@2.1.vendor:64 \
-    android.hardware.biometrics.fingerprint@2.2.vendor:64 \
-    android.hardware.gatekeeper@1.0.vendor \
-    android.hardware.identity-V3-ndk.vendor:64
-    android.hardware.keymaster-V3-ndk.vendor:64
-    android.hardware.keymaster@3.0.vendor:32 \
-    android.hardware.keymaster@4.0.vendor:32 \
-    android.hardware.keymaster@4.1.vendor:64 \
-    android.hardware.neuralnetworks@1.0.vendor:64 \
-    android.hardware.neuralnetworks@1.1.vendor:64 \
-    android.hardware.neuralnetworks@1.2.vendor:64 \
-    android.hardware.neuralnetworks@1.3.vendor:64 \
-    android.hardware.oemlock@1.0.vendor:64 \
-    android.hardware.power-V1-ndk.vendor:64
-    android.hardware.radio.config@1.0.vendor:64 \
-    android.hardware.radio.config@1.1.vendor:64 \
-    android.hardware.radio.config@1.2.vendor:64 \
-    android.hardware.radio.deprecated@1.0.vendor:64 \
-    android.hardware.radio@1.2.vendor:64 \
-    android.hardware.radio@1.3.vendor:64 \
-    android.hardware.radio@1.4.vendor:64 \
-    android.hardware.radio@1.5.vendor:64 \
-    android.hardware.rebootescrow-V1-ndk.vendor:64 \
-    android.hardware.secure_element@1.1.vendor:64 \
-    android.hardware.secure_element@1.2.vendor:64 \
-    android.hardware.sensors@1.0.vendor:32 \
-    android.hardware.sensors@2.0.vendor \
-    android.hardware.thermal@1.0.vendor:64 \
-    android.hardware.weaver@1.0.vendor:64 \
-    android.system.net.netd@1.1.vendor:64
-
-# Misc
-PRODUCT_PACKAGES += \
-    libcrypto_utils.vendor:64 \
-    libpng.vendor \
-    libsqlite.vendor \
-    libssl.vendor:32
 
 # Properties
 TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
-
-# VNDK
-PRODUCT_PACKAGES += \
-    libcrypto-v33
