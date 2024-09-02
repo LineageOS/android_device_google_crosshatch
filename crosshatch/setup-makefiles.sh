cce4e74b8ebc0f005ab7906ca6f3169211f052b3
Michael Bestas <mkbestas@lineageos.org>
2024-09-02 17:09:14 +0300

    b1c1: fix libprotobuf-cpp-lite-21.12 ELF check
    Change-Id: I028723e6ff9ef71514857d683e2c65caf9e28fa0

--- crosshatch/setup-makefiles.sh
+++ crosshatch/setup-makefiles.sh
@@ -42,6 +42,9 @@ function lib_to_package_fixup_vendor_variants() {
             vendor.qti.imsrtpservice@3.0)
             echo "$1_vendor"
             ;;
+        libprotobuf-cpp-lite-21.12)
+            echo "libprotobuf-cpp-lite"
+            ;;
         *)
             return 1
             ;;
