9f725d73a290086ef6a78f2d33973dc71009f74c
Michael Bestas <mkbestas@lineageos.org>
2024-09-02 04:16:01 +0300

    b1c1: fix vendor.qti.hardware.tui_comm@1.0 ELF check
    Change-Id: Iaaa6a2afd002fc4ac50c5b4718c6b2399b04c9de

--- crosshatch/setup-makefiles.sh
+++ crosshatch/setup-makefiles.sh
@@ -38,7 +38,8 @@ function lib_to_package_fixup_vendor_variants() {
     fi
 
     case "$1" in
-        vendor.qti.imsrtpservice@3.0)
+        vendor.qti.hardware.tui_comm@1.0 | \
+            vendor.qti.imsrtpservice@3.0)
             echo "$1_vendor"
             ;;
         *)
