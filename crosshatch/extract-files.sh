db28bcf911b9afb8780bb7af7f7d14f85ca23537
Michael Bestas <mkbestas@lineageos.org>
2024-09-29 11:24:29 +0300

    b1c1: Sync extract scripts with 22 templates
    Change-Id: I7963ae050cfb193c5d0c2636da9c1e8f82bcdbdb

--- crosshatch/extract-files.sh
+++ crosshatch/extract-files.sh
@@ -16,9 +16,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../../.."
 
-# If XML files don't have comments before the XML header, use this flag
-# Can still be used with broken XML files by using blob_fixup
-export TARGET_DISABLE_XML_FIXING=true
+export TARGET_ENABLE_CHECKELF=false
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
