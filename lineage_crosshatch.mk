27fa7390256800517e489e705dbcf0990f9e5407
Michael Bestas <mkbestas@lineageos.org>
2024-10-20 23:09:47 +0300

    b1c1: Adjust override prop key names
    Change-Id: Iaa96fd777196fe90f408457a580b80d86b041140

--- lineage_crosshatch.mk
+++ lineage_crosshatch.mk
@@ -22,9 +22,8 @@ TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=crosshatch \
-    PRIVATE_BUILD_DESC="crosshatch-user 12 SP1A.210812.016.C2 8618562 release-keys"
-
-BUILD_FINGERPRINT := google/crosshatch/crosshatch:12/SP1A.210812.016.C2/8618562:user/release-keys
+    BuildDesc="crosshatch-user 12 SP1A.210812.016.C2 8618562 release-keys" \
+    BuildFingerprint=google/crosshatch/crosshatch:12/SP1A.210812.016.C2/8618562:user/release-keys \
+    DeviceProduct=crosshatch
 
 $(call inherit-product, vendor/google/crosshatch/crosshatch-vendor.mk)
