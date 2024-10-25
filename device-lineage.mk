a096c7f6859948ebb2941045508ff1e46c3afddc
Nolen Johnson <johnsonnolen@gmail.com>
2024-10-25 13:08:35 -0400

    b1c1: Build libcppbor_external.vendor
    * Citadel depends on this.
    
    Change-Id: Ic7d7e54d318e4d6e711ba788f17fabe0508e7a32

--- device-lineage.mk
+++ device-lineage.mk
@@ -101,6 +101,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libkeymaster4support.vendor:64 \
     libkeymaster4_1support.vendor:64 \
+    libcppbor_external.vendor \
     libcrypto_shim
 
 # Json
