5573a6925bfeac4696157fd8ce63f5ae30c9305e
Michael Bestas <mkbestas@lineageos.org>
2024-12-02 00:48:53 +0200

    b1c1: Remove unused thermal-engine headers
    We are using vendor/qcom/opensource/thermal-engine
    
    Change-Id: Id0de11d14b17e6241e8c69aa4425ba8e348c0802

--- sdm845/thermal-engine/Android.mk
+++
@@ -1,6 +0,0 @@
-################################################################################
-# @file device/qcom/common/thermal-engine/Android.mk
-# @Makefile for installing thermal-engine client header on Android.
-################################################################################
-
-LOCAL_PATH:= $(call my-dir)
