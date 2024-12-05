e5fb3adadd17c1f08c323ccabaa756e1e49ac5c5
Nelson Li <nelsonli@google.com>
2024-12-05 03:43:25 +0000

    Change TARGET_RECOVERY_UI_LIB to use fully qualified names in b1c1
    Since the TARGET_RECOVERY_UI_LIB for each device is not exactly the
    same, and most of the modules in it contain `soong_namespace{}` to
    avoid conflicts between different devices, after converting
    `librecovery_ui_ext` from Android.mk to Android.bp, it needs to be
    changed to a `fully qualified name` in order to correctly use the
    corresponding module.
    
    Flag: EXEMPT refactor
    
    Bug: 339143524
    Test: m -j librecovery_ui_ext
    Change-Id: Iee116ad6d0efe7fcc321ec9ecec49224b5ba19e2

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -96,7 +96,7 @@ TARGET_RECOVERY_FSTAB := device/google/crosshatch/fstab.hardware
 endif
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_RECOVERY_UI_LIB := \
-    librecovery_ui_pixel \
+    //hardware/google/pixel/recovery:librecovery_ui_pixel
     libfstab
 
 ifneq ($(filter %_mainline,$(TARGET_PRODUCT)),)
