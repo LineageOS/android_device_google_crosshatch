7bc1308c3832aca0d59c774750e8621d8a5d6cb8
Michael Bestas <mkbestas@gmail.com>
2025-03-08 21:43:32 +0200

    b1c1: Enable ELF checks for libwpa_client dependent libs
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270

--- blueline/extract-files.py
+++ blueline/extract-files.py
@@ -16,7 +16,6 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -43,9 +42,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.tui_comm@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
-    (
-        'libwpa_client',
-    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
--- device.mk
+++ device.mk
@@ -506,7 +506,6 @@ endif
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     wificond \
-    libwpa_client \
     WifiOverlay
 
 # Connectivity
