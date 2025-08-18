8a8c9cd3ec5ae6844bcac992d8c30c62c696d9a5
Nolen Johnson <johnsonnolen@gmail.com>
2025-08-18 15:18:39 -0400

    b1c1: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -15,7 +15,6 @@ TARGET_KERNEL_SOURCE := kernel/google/msm-4.9
 TARGET_NEEDS_DTBOIMAGE := true
 
 # Manifests
-DEVICE_MANIFEST_FILE += device/google/crosshatch/lineage_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
 # Partitions
